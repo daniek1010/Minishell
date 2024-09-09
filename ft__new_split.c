7496d86fb374faaa6c3d6da4e7240de393e28b09
Richard Porth <riporth@c1r10s2.42wolfsburg.de>
2024-09-09 12:48:14 +0200

    only one left

--- ft__new_split.c
+++ ft__new_split.c
@@ -6,7 +6,7 @@
 /*   By: riporth <riporth@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 12:50:03 by riporth           #+#    #+#             */
-/*   Updated: 2024/09/06 17:40:23 by riporth          ###   ########.fr       */
+/*   Updated: 2024/09/09 12:47:03 by riporth          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -195,10 +195,191 @@ char	**ft_token_fill(const char *str, char **list)
 }
 
 //-------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------
 
-char	**ft_token_split(char const *s)
+//-------------------------------------------------------------------------------------------------
+char	*fill_part_one (const char *str, int *j, int i)
+{
+	char	*new_str;
+	int		len;
+
+	len = ft_strlen(str);
+	new_str = (char *)malloc((len + 1) * sizeof(char));
+	new_str[len] = '\0';
+	if (!new_str)
+		return (NULL);
+	while (*j < i)
+	{
+		new_str[*j] = str[*j];
+		*j += 1;
+	}
+	return (new_str);
+}
+
+char	*fill_part_two (const char *str, char *new_str, int j, int i)
+{
+	while (str[i] != '\0')
+	{
+		new_str[j] = str[i];
+		j++;
+		i++;
+	}
+	return (new_str);
+}
+
+char	*remove_char( char *str, char to_remove, int i)
+{
+	char	*new_str;
+	int		j;
+
+	j = 0;
+	new_str = fill_part_one(str, &j, i);
+	if (!new_str)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		if (str[i] != to_remove)
+		{
+			new_str[j] = str[i];
+			j++;
+		}
+		else
+			break ;
+		i++;
+	}
+	if (str[i] != '\0')
+		new_str = fill_part_two(str, new_str, j, i);
+	free(str);
+	return (new_str);
+}
+
+char	*replace_substring(char *str, char *to_replace, char *replacement)
+{
+	char	*pos;
+	char	*new_str;
+	int		new_len;
+	int		prefix_len;
+
+	pos = ft_strstr(str, to_replace);
+	if (!pos)
+		return (str);
+	new_len = ft_strlen(str) - ft_strlen(to_replace) + ft_strlen(replacement);
+	new_str = malloc((new_len + 1) * sizeof(char));
+	if (!new_str)
+		return (str);
+	prefix_len = pos - str;
+	ft_strcpy(new_str, str);
+	new_str[prefix_len] = '\0';
+	ft_strcat(new_str, replacement);
+	ft_strcat(new_str, pos + ft_strlen(to_replace));
+	return (new_str);
+}
+
+char	*isolate_variable(char *str, int i)
+{
+	int		length;
+	int		start;
+	char	*var;
+
+	if (str[i] != '$')
+		return (NULL);
+	i++;
+	start = i;
+	while (is_valid_var_char(str[i]))
+		i++;
+	length = i - start;
+	if (length == 0)
+		return (NULL);
+	var = malloc((length + 1) * sizeof(char));
+	if (!var)
+		return (NULL);
+	strncpy(var, str + start, length);
+	var[length] = '\0';
+	return (var);
+}
+
+char	*expand_var(t_infos *tokens, char *str, int x, int *flag)
+{
+	char	*var;
+	char	*replace;
+
+	var = isolate_variable(str, x);
+	x++;
+	if (str[x] == '?')
+	{
+		replace = ft_strdup(ft_itoa(tokens->e_code));
+		*flag = 1;
+	}
+	else
+		replace = get_env_var(*(tokens->envp), var);
+	return (replace);
+}
+
+char	*add_var(char *str, int *i, t_infos *tokens)
+{
+	char	*new;
+	char	*var;
+	char	*replace;
+	int		flag;
+	char	*new_var;
+
+	flag = 0;
+	var = isolate_variable(str, *i);
+	replace = expand_var(tokens, str, *i, &flag);
+	new_var = ft_strjoin("$", var);
+	if (!replace)
+	{
+		*i += ft_strlen(var);
+		return (str);
+	}
+	new = replace_substring(str, new_var, replace);
+	free (var);
+	free (new_var);
+	*i += ft_strlen(replace);
+	free (replace);
+	free(str);
+	return (new);
+}
+
+char	*ft_extract_variables(char *str, t_infos *tokens)
+{
+	int	i;
+	int	in_qoute_1;
+	int	in_qoute_2;
+
+	i = 0;
+	in_qoute_1 = 2;
+	in_qoute_2 = 2;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\'' && in_qoute_1 % 2 == 0)
+			in_qoute_2 += 1;
+		else if (str[i] == '\"' && in_qoute_2 % 2 == 0)
+			in_qoute_1 += 1;
+		if (str[i] == '$' && in_qoute_2 % 2 == 0)
+			str = add_var(str, &i, tokens);
+		else
+			i++;
+	}
+	return (str);
+}
+
+char	**ft_var_up(char **list, int count, t_infos *tokens)
+{
+	int	x;
+
+	x = 0;
+	while (x != count)
+	{
+		list[x] = ft_extract_variables(list[x], tokens);
+		//list[x] = ft_remove_qoutation();
+		x++;
+	}
+	return (list);
+}
+
+//-------------------------------------------------------------------------------------------------
+// include t_infos *tokens
+char	**ft_token_split(char const *s, t_infos *tokens)
 {
 	int		word;
 	char	**list;
@@ -209,27 +390,6 @@ char	**ft_token_split(char const *s)
 		return (NULL);
 	list[word] = NULL;
 	list = ft_token_fill(s, list);
-	// cut function and $ fill
+	list = ft_var_up(list, word);
 	return (list);
-}
-
-
-int	main(void)
-{
-	const char *test_str = "Hello 'world >'|test\"exam<ple\" <>";
-	char **tokens;
-	int i = 0;
-
-	tokens = ft_token_split(test_str);
-
-	// Print each token
-	printf("Tokens:\n");
-	while (tokens[i] != NULL)
-	{
-		printf("Token %d: %s\n", i + 1, tokens[i]);
-		free(tokens[i]);
-		i++;
-	}
-	free(tokens);
-	return (0);
 }
